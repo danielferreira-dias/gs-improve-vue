c66d0fa23268667f8553d0c81f7c04bd42e3abb8
danielferreira-dias <142907197+danielferreira-dias@users.noreply.github.com>
2024-10-21 23:11:00 +0100

    small change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 
 
 <template>
- <div class="justify-center flex  items-center h-screen">
-    <div class="text-4xl">
+ <div class="justify-center flex  items-center h-screen bg-gradient-to-b from-fuchsia-900 from-0% via-black via-80% to-black">
+    <div class="text-4xl text-white">
       Hello World
     </div>
   </div>
