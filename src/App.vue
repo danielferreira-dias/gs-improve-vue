6aa43e3834b5e852b63ee3d145d77fe06737d975
danielferreira-dias <142907197+danielferreira-dias@users.noreply.github.com>
2024-10-21 23:19:48 +0100

    Small commit

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
 
 <template>
  <div class="justify-center flex  items-center h-screen bg-gradient-to-b from-fuchsia-900 from-0% via-black via-80% to-black">
-    <div class="text-4xl text-white">
-      Hello World
-    </div>
+  <div class="w-2/3 h-[75%] bg-white rounded-lg flex flex-col text-center p-2">
+    <div class="w-full h-fit text-4xl font-bold">AICare Survey</div>
+  </div>
+    
   </div>
 </template>
